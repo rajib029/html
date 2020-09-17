b278d848fc43c66dfe75aa1edc48891ac96a87e7
MH Rajib <rajib029@gmail.com>
2020-09-18 00:48:54 +0600

    Add files via upload

---
+++ neumeric-html/js/custom.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+
+// Can also be used with $(document).ready()
+$(window).load(function() {
+
+  $(document).ready(function() {
+        $('.menu-btn').bigSlide();
+
+
+});
+	
+	$('.flexslider').flexslider({
+    animation: "fade",
+	slideshowSpeed: 4000
+  });
+	
+});
---
+++ parkview/js/custom.js
@@ -0,0 +1,13 @@
+
+
+$(window).load(function() {
+
+  $(".mobile-menu").click(function(){
+		$(".mobile-nav").slideToggle(350);
+	});
+	
+  $('.myflex-1').flexslider({
+    animation: "slide"
+  });
+  });
+  
